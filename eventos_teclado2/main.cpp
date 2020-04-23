d2573f1c0b8aa076a8c8d55588f593b3d0b78486
Alxndr3 <alxndr3@gmail.com>
2020-04-23 19:30:53 -0300

    Commit3

---
+++ eventos_teclado2/main.cpp
@@ -0,0 +1,56 @@
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <iostream>
+
+
+void evento_teclado01(bool continuar, ALLEGRO_EVENT_QUEUE *fila_eventos, ALLEGRO_EVENT event) {
+	// Loop principal
+	while(continuar) {
+		al_wait_for_event(fila_eventos, &event);
+		if(event.type == ALLEGRO_EVENT_KEY_DOWN) {
+			if(event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+				continuar = false;
+				std::cout << "A tecla Esc foi pressionada" << std::endl;
+				std::cout << "Este Loop será encerrado!" << std::endl;
+			}
+		}
+	}
+}
+
+int main() {
+	int tela_largura = 800;
+	int tela_altura = 600;
+	bool continuar = true;
+
+	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+	ALLEGRO_EVENT event;
+	ALLEGRO_DISPLAY *tela = NULL;
+	// Inicialização da Allegro5
+	al_init();
+	// Inicialização dos Addons
+	al_init_primitives_addon();
+	al_install_keyboard();
+	// Geração da tela
+	tela = al_create_display(tela_largura, tela_altura);
+	// Cria fila de eventos
+	fila_eventos = al_create_event_queue();
+	// Registrar os tipos de eventos que queremos captar na fila de enventos criada logo acima
+	// **Neste caso estamos indicando que qualquer tecla pressionada será capturada na fila de eventos
+	al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+	// Define cores
+	ALLEGRO_COLOR cor_fundo = al_map_rgb(0, 0, 0);
+	ALLEGRO_COLOR cor_quadrado = al_map_rgb(0, 255, 0);
+	// Limpa tela e apresenta cor de fundo
+	al_clear_to_color(cor_fundo);
+	// Desenha retângulo
+	al_draw_rectangle(20, 20, 50, 50, cor_quadrado, 2);
+	// Apresenta tela
+	al_flip_display();
+
+	evento_teclado01(continuar, fila_eventos, event);
+
+	// Destrói os ponteiros criados para evitar o consumo desnecessário de memória
+	al_destroy_display(tela);
+
+	return 0;
+}
